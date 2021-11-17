c97a861f955baa00342a6510d648561726fcfdf9
Florent Guilbaud <florent.guilbaud@gmail.com>
2021-11-17 09:16:14 +0100

    refacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # INDEX
+  get "pets", to: "pets#index"
+  get "pets/new", to: "pets#new", as: :new_pet
+  get "pets/:id", to: "pets#show", as: :pet
+  # [POST] "/pets"
+  post "pets", to: "pets#create"
+
+  get "pets/:id/edit", to: "pets#edit", as: :edit_pet
+  # [PATCH] "/pets/1"
+  patch "pets/:id", to: "pets#update"
+
+  delete "pets/:id", to: "pets#destroy"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Pet.create(name: "Titi", address: "Nantes", found_on: Time.now, species: "t-rex")
+Pet.create(name: "Martin", address: "Rennes", found_on: Time.now - 1.day, species: "ninja turtle")
+Pet.create(name: "Toto", address: "Paris", found_on: Time.now - 10.day, species: "dog")
+Pet.create(name: "Nemo", address: "Océan", found_on: Time.now + 20.day, species: "fish")
